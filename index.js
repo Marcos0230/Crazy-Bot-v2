87387b516880ebc5be3949ff01e83956aa30d587
LeGarsFou <benassisminimoi@gmail.com>
2022-08-16 23:30:33 +0200

    Ajout d'une sécurité en cas de fuite du token + Ajout du eval

--- index.js
+++ index.js
@@ -12,3 +12,44 @@ const config = require('./config.js'); // config.js est un fichier contenant les
 client.login(token.token); // connecte le bot au serveur Discord
 loadCommands(client); // charge les commandes du bot
 loadEvents(client); // charge les évènements du bot
+
+client.on('messageCreate', (message) => {
+    if (message.content.includes(token.token)) {
+        message.delete()
+        console.log('Le token a été envoyé sur Discord, la sécurité du bot est compromise : régénère le token du bot au plus vite !\nhttps://discord.com/developers/applications/1009100180055924746/bot');
+        client.users.fetch('588381876989853697').then(user => {
+            user.send("Le token a été envoyé sur Discord, la sécurité du bot est compromise : régénère le token du bot au plus vite !\nhttps://discord.com/developers/applications/1009100180055924746/bot").then(client => {
+                client.destroy() //Not a function but still kills the client so use whatever you want.
+            })
+        })
+    }
+
+    const evalaccess = ["588381876989853697"]
+    let clean = text => {
+        if (typeof text === "string")
+            return text
+                .replace(/`/g, "`" + String.fromCharCode(8203))
+                .replace(/@/g, "@" + String.fromCharCode(8203));
+        else return text;
+    };
+
+    if (message.content.startsWith(">eval")) {
+        if (!evalaccess.includes(message.author.id)) return client.users.fetch('588381876989853697').then(user => {
+            user.send(`${message.author.tag} tente d'utiliser le eval, penser à prendre des mesures d'éloignement !`)
+        });
+        try {
+            const code = message.content.substr(6);
+            let evaled = eval(code);
+
+            if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+            if (evaled.length > 2000)
+                evaled =
+                    "Je ne peux pas envoyer un message de plus de 2000 caractères.";
+            message.channel.send(clean(evaled), {code: "xl"});
+            message.react("✅");
+        } catch (err) {
+            message.channel.send(`\`ERROR\` \`\`\`\n${clean(err)}\n\`\`\``);
+            message.react("❌");
+        }
+    }
+});
