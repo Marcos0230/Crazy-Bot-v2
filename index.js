8a562d73a50770beb68500a3c364934f291c6348
LeGarsFou <benassisminimoi@gmail.com>
2022-08-20 00:26:05 +0200

    Ajout d'un statut au bot

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { ActivityType } = require('discord.js');
 const intents = new Discord.IntentsBitField(3276799);
 const client = new Discord.Client({intents});
 const loadCommands = require('./Loader/loadCommands.js');
@@ -10,3 +11,23 @@ client.commands = new Discord.Collection();
 client.login(token.token) // On se connecte au bot
 loadCommands(client); // On charge les commandes
 loadEvents(client); // On charge les events
+
+client.on('ready', () => {
+    const statuses = [
+        'mon développeur : 🦄𝐋𝐞𝐆𝐚𝐫𝐬𝐅𝐨𝐮🌈#0230',
+        'mon prefix : c.',
+        'ma version : 2.0.0'
+    ]
+    let i = 0;
+    /*client.user.setPresence({
+        activities: [{ name: `discord.js v14`, type: ActivityType.Watching }],
+        status: 'dnd',
+    });*/
+    setInterval(() => {
+        client.user.setPresence({
+            activities: [{ name: statuses[i], type: ActivityType.Watching }],
+            status: 'online',
+        });
+        i = ++i % statuses.length
+    }, 10000)
+})
