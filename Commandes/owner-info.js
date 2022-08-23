1b9a1e6c389ce6035ec56352a3df19321f9ae2e5
LeGarsFou <benassisminimoi@gmail.com>
2022-08-23 19:22:43 +0200

    Ajout de la commande owner-info

---
+++ Commandes/owner-info.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const {EmbedBuilder} = require("discord.js");
+
+module.exports = {
+    name: "owner-info",
+    description: "Permet de connaître des infos sur le créateur du bot",
+    dmPermission: true,
+    permission: "Aucune",
+
+    async run(client, message) {
+        const owner = client.users.cache.get("588381876989853697");
+
+        const ownerInfoEmbed = new EmbedBuilder()
+            .setTitle("Créateur Info")
+            .setDescription(`Voici les infos sur le créateur du bot :`)
+            .setColor("Random")
+            .setTimestamp()
+            .setThumbnail(owner.displayAvatarURL({dynamic: true}))
+            .setFooter({text: "Commande : owner-info", iconURL: client.user.displayAvatarURL({dynamic: true})})
+            .addFields({
+                name: ":clipboard: Pseudo",
+                value: `${owner.username}`,
+                inline: true
+            })
+            .addFields({
+                name: ":id: ID",
+                value: `${owner.id}`,
+                inline: true
+            })
+            .addFields({
+                name: ":1234: Tag",
+                value: `#${owner.discriminator}`,
+                inline: true
+            })
+            .addFields({
+                name: ":map: Localisation",
+                value: "France :flag_fr:",
+                inline: true
+            })
+
+        await message.reply({embeds: [ownerInfoEmbed]});
+    }
+}
