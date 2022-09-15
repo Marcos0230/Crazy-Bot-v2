884556571232b16aa2163b5f529a041e409e623e
LeGarsFou <benassisminimoi@gmail.com>
2022-09-15 20:47:24 +0200

    Améliorations

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports = async (client, interaction) => {
-    if (interaction.type === Discord.InteractionType.ApplicationCommand) {
+        if (interaction.type === Discord.InteractionType.ApplicationCommand) {
         let command = require(`../Commandes/${interaction.commandName}`);
         if (!command || !interaction.commandName) {
             interaction.reply("Pas de commande renseignée ! / Commande inconnue !");
